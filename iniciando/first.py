49028401d77d96a4511eb744182649a852181bdd
sergiozuloaga10 <sergio-sst@hotmail.com>
2023-12-26 22:14:04 -0600

    primer dia

---
+++ iniciando/first.py
@@ -0,0 +1,9 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+ft.app(target=main)
+
+#continuará
